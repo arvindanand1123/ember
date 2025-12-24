6648cc4df231ed793ddd0c82f02b3dcb6ab7220c
arvind <arvindanand1123@gmail.com>
2025-12-24 15:52:35 -0800

    Add app test (#43)
    c

---
+++ tests/pages/App.test.tsx
@@ -0,0 +1,31 @@
+import { clearMocks } from '@tauri-apps/api/mocks';
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterEach, assert, beforeEach, describe, it } from 'vitest';
+
+import App from '../../src/App';
+import { setupTauriMocks } from '../mocks';
+import { clickButton } from '../utils';
+
+beforeEach(() => {
+  setupTauriMocks('../phorgePDF/tests/basic.pdf');
+});
+
+afterEach(() => {
+  cleanup();
+  clearMocks();
+});
+
+describe('App', () => {
+  it('file select', async () => {
+    render(<App/>);
+    assert(screen.getByText('Select File'));
+    await clickButton('Select File');
+  });
+
+  it('pdf view', async () => {
+    render(<App/>);
+    await clickButton('Select File');
+    await clickButton('Back');
+    assert(screen.getByText('Select File'));
+  });
+});
