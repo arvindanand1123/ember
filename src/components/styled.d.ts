6c8970737c07c51fac8a3995877dc67778360f8d
arvindanand1123 <arvindanand1123@gmail.com>
2026-08-19 14:38:27 -0700

    checkpoint

--- src/components/styled.d.ts
+++ src/components/styled.d.ts
@@ -3,6 +3,7 @@ import 'styled-components';
 import { Theme } from './theme';
 
 declare module 'styled-components' {
-  // eslint-disable-next-line
+  // module augmentation: DefaultTheme must inherit Theme's members without adding any
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
